8ed918a4675dd4ffb42707c9fe6c8329684ca405
radchenkoed <radchenkoed@sgu.ru>
2023-09-20 00:22:33 +0400

    feat: brackets task

---
+++ src/dist/index.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function check(str, bracketsConfig) {
+  var flag = true;
+  bracketsConfig.forEach(function (element, index) {
+    var occurs = countOccur(str, element, bracketsConfig);
+    if (occurs[0] > 0 || occurs[2] % 2 != 0) flag = false;
+  });
+  return flag;
+};
+
+function countOccur(str, element, config) {
+  return str.split('').reduce(function (acc, curr, ind) {
+    if (element[0] == curr && acc[1] % 2 == 0) {
+      acc[0] += 1;
+      if (element[0] == element[1]) acc[0] -= 1;
+    } else if ((element[1] == curr || element[1] == element[0]) && ind < str.length && acc[0] > 0 && acc[1] % 2 == 0) {
+      acc[0] -= 1;
+    } else if (!element.includes(curr)) {
+      acc[1] += 1;
+    } else {
+      acc[2] += 1;
+    }
+
+    return acc;
+  }, [0, 0, 0]);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true
+
+  bracketsConfig.forEach((element, index) => {
+    let occurs = countOccur(str, element, bracketsConfig)
+    if (occurs[0] > 0 || occurs[2] % 2 != 0 ) flag = false
+  })
+  return flag
+}
+
+
+function countOccur(str, element, config) {
+  return str.split('').reduce((acc, curr, ind) => {
+    if (element[0] == curr && acc[1] % 2 == 0) {
+      acc[0] += 1
+      if (element[0] == element[1]) acc[0] -= 1
+    }
+    else if ((element[1] == curr || element[1] == element[0]) && ind < str.length && acc[0] > 0 && acc[1] % 2 == 0) {
+      acc[0] -= 1
+    }
+    else if (!element.includes(curr)) {
+      acc[1] += 1
+    }
+    else {
+      acc[2] += 1
+    }
+    return acc;
+  }, [0, 0, 0])
 }
